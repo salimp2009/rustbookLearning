1f6d9bbb401ebb53c627dc4d25364c6d76f748b9
salimp2009@gmail.com <salimp2009@gmail.com>
2022-12-17 00:05:00 +0300

    added some examples of String, Slices and &str

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,38 @@
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
+
+fn first_word_sliced(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    s
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("demiros");
+    let length_or_last = first_word(&s);
+    s.clear();
+    println!("length or last index: {length_or_last}");
+    s.push_str("Hello World");
+    let hello = &s[..5];
+    let world = &s[5..];
+    let hello_world = &s[..];
+    println!("{hello}{world}");
+    println!("{hello_world}");
+    s.insert_str(0, "hoops ");
+
+    let word1 = first_word_sliced(&s);
+    println!("sliced string first word : {:?}", word1);
 }
