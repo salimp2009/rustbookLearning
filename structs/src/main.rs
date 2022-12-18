7a8be700e7c1da981ba8fba55cc15cd6e563339c
salimp2009@gmail.com <salimp2009@gmail.com>
2022-12-18 14:20:42 +0300

    added examples of setting, changing, creating and update Struct; next is Tuple Structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -7,12 +7,46 @@ struct User {
     sign_in_count: u64,
 }
 
+#[allow(dead_code)]
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         active: true,
         email: String::from("blabla@zort.com"),
         username: String::from("blabla"),
         sign_in_count: 1,
     };
     println!("User info: {:?} ", user1);
+    println!("user1 email: {}", user1.email);
+    println!("user1 username: {}", user1.username);
+
+    let mut user2 = User {
+        active: true,
+        email: String::from("blabla@zort.com"),
+        username: String::from("demiros"),
+        sign_in_count: 1,
+    };
+
+    user2.email = String::from("demrios@demiros.com");
+    println!("user2: {:?}", user2);
+
+    let user3 = User {
+        email: String::from("didolos@homikos.com"),
+        // username: String::from("didolos"),
+        ..user1
+    };
+    println!("user3: {:?}", user3);
+    assert_eq!(user1.sign_in_count, 1);
+    // we can username of user1 since String is moved but other fields are valid
+    //    assert_eq!(user1.username, String::from("blabla"));
+    user1.username = String::from("yowww_yowww");
+    println!("user1 after the move : {:?}", user1);
 }
