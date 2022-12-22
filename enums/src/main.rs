bcd1d76d5d92f564c31e75b0cc8b925be0fcddaf
salimp2009@gmail.com <salimp2009@gmail.com>
2022-12-22 20:03:37 +0300

    added more example to use enum and if let flow

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -20,11 +20,33 @@ enum IpAddrNew {
     V6(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 #[allow(dead_code)]
 enum IpAdrNew2 {
     V4(Ipv4Addr),
     V6(Ipv6Addr),
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum IpAdrNew3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Message {
+    Quit,                       // unit struct
+    Move { x: i32, y: i32 },    // struct
+    Write(String),              // tuple struct
+    ChangeColor(i32, i32, i32), // like a tuple struct; struct Color(...)
+}
+
+#[allow(dead_code)]
+impl Message {
+    fn call(&self) { /* to be implemented */
+    }
 }
 
 fn route(ip_kind: IpAddrKind) {
@@ -50,11 +72,36 @@ fn main() {
 
     let localhost = Ipv4Addr::new(127, 0, 0, 1);
     assert_eq!("127.0.0.1".parse(), Ok(localhost));
-    assert_eq!(localhost.is_loopback(), true);
+    assert!(localhost.is_loopback());
     assert!("012.004.002.000".parse::<Ipv4Addr>().is_err()); // all octets are in octal
     assert!("0000000.0.0.0".parse::<Ipv4Addr>().is_err()); // first octet is a zero in octal
     assert!("0xcb.0x0.0x71.0x00".parse::<Ipv4Addr>().is_err()); // all octets are in hex
                                                                 //
     let new_home = IpAdrNew2::V4(Ipv4Addr::new(127, 0, 0, 1));
     println!("new_home : {:?}", new_home);
+
+    let home_new2 = IpAdrNew3::V4(127, 0, 0, 1);
+    println!("new_home2: {:?}", home_new2);
+
+    let message1 = Message::Write(String::from("first commit"));
+    message1.call();
+
+    let _some_number = Some(25);
+    let some_char = Some('r');
+
+    let result = some_char.map_or('E', |x| x);
+    println!("result some_char: {result}");
+
+    let x = Some("foo");
+    assert_eq!(x.map_or(42, |v| v.len()), 3);
+
+    let x: Option<&str> = None;
+    assert_eq!(x.map_or(42, |v| v.len()), 42);
+    let absent_number: Option<i32> = None;
+
+    if let Some(x) = absent_number {
+        println!("Some(x): {x}");
+    } else {
+        println!("no value(None)");
+    }
 }
