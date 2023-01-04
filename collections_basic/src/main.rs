cfad6ad540aed1ea1bc4e2e9dab646d5e9df98ff
salimp2009@gmail.com <salimp2009@gmail.com>
2023-01-04 13:48:29 +0300

    added example for using enum with in vector to store different types; if the types are unknows then a Vector<&dyn Trait> ; a Trait object

--- collections_basic/src/main.rs
+++ collections_basic/src/main.rs
@@ -1,3 +1,10 @@
+#[derive(Debug, PartialEq, PartialOrd)]
+enum SpreadsheetCells {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
 fn main() {
     let mut v: Vec<i32> = Vec::new();
     v.push(5);
@@ -38,11 +45,29 @@ fn main() {
     for i in &mut v {
         *i += 10;
     }
-
     println!("{:?}", v);
+
+    // prefer this over nor for in {} loop for performance reason
+    // this is way better
     v.iter_mut().for_each(|i| {
         *i += 10;
     });
+    println!("after adding +10: {:?}", v);
 
-    println!("{:?}", v);
+    // copies value within the vector from the specified range
+    v.extend_from_within(2..4);
+    println!("after the extend: {:?}", v);
+
+    v.extend_from_slice(&[100, 150]);
+    println!("after adding from slice: {:?}", v);
+
+    let row = vec![
+        SpreadsheetCells::Int(3),
+        SpreadsheetCells::Text(String::from("blue")),
+        SpreadsheetCells::Float(10.12),
+    ];
+
+    println!("SpreadsheetCells: {:?}", row);
+
+    assert_eq!(row.get(0), Some(&SpreadsheetCells::Int(3)));
 }
