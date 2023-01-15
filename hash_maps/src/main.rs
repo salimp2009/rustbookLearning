8f76bc2edb8a676ed7d0e1588c462377e72b3ca4
salimp2009@gmail.com <salimp2009@gmail.com>
2023-01-15 14:56:01 +0300

    added some examples of adding key value into hashmap, iterate over it and get value for an existing key using get

--- hash_maps/src/main.rs
+++ hash_maps/src/main.rs
@@ -4,4 +4,18 @@ fn main() {
     let mut scores = HashMap::new();
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 15);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    assert_eq!(score, 10);
+    let solar_distance = HashMap::from([
+        ("Mercury", 0.4),
+        ("Venus", 0.7),
+        ("Earth", 1.0),
+        ("Mars", 1.5),
+    ]);
+    println!("solar distance: {:?}", solar_distance);
+    solar_distance
+        .iter()
+        .for_each(|(k, v)| println!("{k}: {v}"));
 }
